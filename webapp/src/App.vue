766378353de3f291305d21ac3a56d844d1cafe7e
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-06-11 00:07:41 -0300

    basic vue webapp setup

---
+++ webapp/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ComponentExample from './components/componentExample.vue';
+
+@Component({
+  components: {
+    ComponentExample,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+</style>
